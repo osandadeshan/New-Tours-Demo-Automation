d687022766379d935564ea4643c3ae3339f918e7
Osanda Deshan <osanda.deshan@gmail.com>
2018-09-30 17:00:47 +0530

    Adding generated executable docs

---
+++ docs/html/data.js
@@ -0,0 +1,2 @@
+var tags = []
+var specs = [{"path":"specs\\Login - Negative Tests.html","name":"Login - Negative Tests Specification","scenarios":[{"name":"Login with invalid credentials","tags":[]}]},{"path":"specs\\Login - Positive Tests.html","name":"Login - Positive Tests Specification","scenarios":[{"name":"Login with valid username and valid password","tags":[]}]}]
